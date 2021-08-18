d75d5db00e80e840eb59b4c5c2dc86d61695afb5
Aimee Kerr <akerr@roos.com>
2021-08-18 11:06:24 -0700

    Table controls

--- src/components/BuildTable.js
+++ src/components/BuildTable.js
@@ -5,7 +5,7 @@ import GlobalFilter from './GlobalFilter';
 import ColumnFilter from './ColumnFilter';
 
 function BuildTable(props) {
-    const data = props.data;
+    let data = props.data;
     console.info('after', data);
     //console.info('in BuildTable: ', props.data2)
 
@@ -62,11 +62,14 @@ function BuildTable(props) {
         canNextPage,
         canPreviousPage,
         pageOptions,
+        gotoPage,
+        pageCount,
+        setPageSize,
         prepareRow,
         state,
         setGlobalFilter,
     } = useTable({ columns, data, defaultColumn, globalFilter }, useFilters, useGlobalFilter, usePagination, useFlexLayout)
-    const { globalFilter, pageIndex } = state
+    const { globalFilter, pageIndex, pageSize } = state
 
     return (
       <>
@@ -107,15 +110,42 @@ function BuildTable(props) {
          })}
        </tbody>
      </table>
-     <div> 
-         <span>
-            Page{' '}
-            <strong>
-              {pageIndex + 1} of {pageOptions.length}
-            </strong>{' '}
-         </span>
-         <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
-         <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+     <div className='controls'> 
+      <div className='controls1'>
+        <span className='spaceright'>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page: {' '}
+          <input type='number' defaultValue={pageIndex + 1}
+            onChange={e => {
+              const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(pageNumber)
+            }}
+            style={{ width: '50px' }}
+            />
+        </span>
+      </div>
+      <div className='controls1'>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{'>>'}</button>
+      </div>
+      <div className='controls1'>
+        <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+          {
+            [10, 25, 50, 100].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))
+          }
+        </select>
+      </div>
      </div>
      </>
     )
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default class Statistics extends React.Component {
-    render() {
+function Statistics(props) {
+        const data = props.data;
+        let i,countPass = 0,countFail = 0,avgTime = 0,timeCount = 0;
+        for(i=0;i<data.length;i++)
+        {
+            if(data[i].value == 'PASS' && data[i].name == 'PF')
+            {
+                countPass++;
+            }
+            if(data[i].value == 'FAIL' && data[i].name == 'PF')
+            {
+                countFail++;
+            }
+            if(data[i].name == 'EVENT_TEST_TIME')
+            {
+                avgTime = avgTime + data[i].value;
+                timeCount++;
+            }
+        }
+        avgTime = avgTime/timeCount;
         return (
-            <h3>Future Content</h3>
+            <div>
+                <h3>Passed:</h3>
+                {countPass}
+                <h3>Failed:</h3>
+                {countFail}
+                <h3>Average Event Test Time:</h3>
+                {avgTime}
+            </div>
         );
-    }
 }
+
+function mapStateToProps(state) {
+    console.log('in func',state.data);
+    return {
+      data: state.data
+    }
+  }
+  
+  export default connect(mapStateToProps, { })(Statistics);
