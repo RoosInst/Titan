1e65656fada8b6015bef3c5ca4c8f0444e403ae9
Aimee Kerr <akerr@roos.com>
2021-09-17 13:24:25 -0700

    small change

--- src/components/Part.js
+++ src/components/Part.js
@@ -18,11 +18,11 @@ class Part extends React.Component {
 const mapStateToProps = (state, ownProps) => {
     const { partNum } = ownProps;
     return {
-        partId: state.data.parts_id[0] ? state.data.parts_id[0].values[partNum][0] : 'Nothing',
-        result: state.data.result[0] ? state.data.result[0].values[partNum][0] : 'Nothing',
-        testTime: state.data.testTime[0] ? state.data.testTime[0].values[partNum][0] : 'Nothing',
-        cycleTime: state.data.cycleTime[0] ? state.data.cycleTime[0].values[partNum][0] : 'Nothing',
-        site: state.data.site[0] ? state.data.site[0].values[partNum][0] : 'Nothing',
+        partId: state.data.parts_id[0] ? state.data.parts_id[0].values[partNum][0] : '--',
+        result: state.data.result[0] ? state.data.result[0].values[partNum][0] : '--',
+        testTime: state.data.testTime[0] ? state.data.testTime[0].values[partNum][0] : '--',
+        cycleTime: state.data.cycleTime[0] ? state.data.cycleTime[0].values[partNum][0] : '--',
+        site: state.data.site[0] ? state.data.site[0].values[partNum][0] : '--',
     }
     
 };
